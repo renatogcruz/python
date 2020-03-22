4c5aa9018571c911859c465400e06d322b13d2ad
renatogcruz <renatogcruz@hotmail.com>
2020-03-22 14:59:53 -0300

    revisando sorted, sort, etc

---
+++ curso_intesivo_python/listas/cars.py
@@ -0,0 +1,27 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+#cars.sort() #altera a ordem dos carros para ordem alfabética
+#print(cars)
+
+#cars.sort(reverse=True) #ordem alfabética inversa
+#print(cars)
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:") 
+print(sorted(cars)) #permite exibir uma lista em uma ordem particular sem afetar a ordem original da lista
+
+print("\nHere is the original again:")
+print(cars)
+
+#DESCOBRINDO TAMANHO DE LISTAS
+tamanho = len(cars)
+print(tamanho)
+
+
+
+
+
+
+ 
