4320304d7ad614ee79eae66e410fb0bbc25286f0
renatogcruz <renatogcruz@hotmail.com>
2020-03-24 19:54:34 -0300

    and e or

---
+++ curso_intesivo_python/instrucao_if/toppingss.py
@@ -0,0 +1,20 @@
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+	print("Hold the anchovies!")
+
+print('mushrooms' in requested_topping)
+print('pineapple' in requested_topping)
+
+answer = 17
+
+if answer != 42:
+	print("That is not the corret answer. Please try again!")
+
+age_0 = 22
+age_1 = 18
+print(age_0 >= 21 and age_1 >= 21)
+print(age_0 >= 21 or age_1 >= 21)
+
+
+ 
