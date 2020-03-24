8b80e8f9ede75b8523def7428e1bc815a9278a1d
renatogcruz <renatogcruz@hotmail.com>
2020-03-24 19:46:04 -0300

    instrução if

---
+++ curso_intesivo_python/instrucao_if/cars.py
@@ -0,0 +1,15 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+	if car == 'bmw':
+		print(car.upper())
+	else:
+		print(car.title())
+
+car = 'bmw'
+print(car == 'bmw') #teste condicional
+print(car == 'subaru')
+print(car.upper() == 'BMW')
+
+
+ 
