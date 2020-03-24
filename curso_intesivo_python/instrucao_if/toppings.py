676bc857fd69fe738271d9fdcd5aed50f14889f2
renatogcruz <renatogcruz@hotmail.com>
2020-03-24 20:46:51 -0300

    if and if or if and elif

---
+++ curso_intesivo_python/instrucao_if/toppings.py
@@ -0,0 +1,10 @@
+request_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in request_toppings:
+	print("Adding mushrooms.")
+if 'pepperoni' in request_toppings:
+	print("Adding pepperoni")
+if 'extra cheese' in request_toppings:
+	print("Ading extra cheese")
+
+print("\nFinished making your pizza!")
