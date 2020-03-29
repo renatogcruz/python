551d5327f19b01e766c551dfd2b734ec194cf062
renatogcruz <renatogcruz@hotmail.com>
2020-03-28 21:20:51 -0300

    revisando while

---
+++ curso_intesivo_python/while/greeter.py
@@ -0,0 +1,8 @@
+prompt = "If you tell us who you are, we can personlaize the messages you see."
+
+prompt += "\nWhat is your first name?"
+
+name = input(prompt)
+print("\nHello, " + name + "!")
+
+age = input("How old are you?")
