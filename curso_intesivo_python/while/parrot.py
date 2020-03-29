fcedad9b240da3f56a6f973be1b1c95be127c5ba
renatogcruz <renatogcruz@hotmail.com>
2020-03-29 17:54:19 -0300

    trabalhando com while

---
+++ curso_intesivo_python/while/parrot.py
@@ -0,0 +1,11 @@
+prompt = "\nTell me something, and I will repeat it back yo you:"
+
+prompt += "\nEnter 'wuit' to end the program"
+
+message = ""
+
+while message != 'quit':
+	message = input(prompt)
+
+print(message)
+
