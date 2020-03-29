3b2244fb6a63e83fcee31da2008a499addfd43c7
renatogcruz <renatogcruz@hotmail.com>
2020-03-29 18:17:03 -0300

    lista

---
+++ curso_intesivo_python/while/confirmed_users.py
@@ -0,0 +1,10 @@
+unconfirmed_users = ['alice', 'brian', 'candace']
+
+confirmed_users = []
+
+while unconfirmed_users:
+	current_user = unconfirmed_users.pop()
+	print("Verifying user: " + current_user.title())
+	confirmed_users.append(current_user)
+
+print("\nThe following users have been confirmed:")
