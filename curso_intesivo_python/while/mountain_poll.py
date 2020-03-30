1a85c4dc7713889eb12d6c5936d8e1ed6b248880
renatogcruz <renatogcruz@hotmail.com>
2020-03-30 19:38:18 -0300

    preenchendo um dicionário com dados de entrada do usuário

---
+++ curso_intesivo_python/while/mountain_poll.py
@@ -0,0 +1,12 @@
+responses = {}
+
+polling_active = True
+
+name = input("\nWhat is your name? ")
+responde = input("Wich mountain would you like to climb someday?")
+
+responses[names] = response
+repeat = input("Would you like to let another person respond? (yes/ no")
+if reapeat == 'no':
+	polling_active = False
+	print("\n--- Poll Results---")
