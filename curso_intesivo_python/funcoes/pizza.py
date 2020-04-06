cec99a715a01699f373537d4a4724009e11fcfcf
renatogcruz <renatogcruz@hotmail.com>
2020-04-06 19:44:29 -0300

    misturando argumentos posicionais e arbitrários

--- curso_intesivo_python/funcoes/pizza.py
+++ curso_intesivo_python/funcoes/pizza.py
@@ -1,9 +1,16 @@
 def make_pizza(*toppings): #o asterisco no nome diz a python para criar uma tupla vazia chamada toppings e reunir os valores recebidos nessa tupla
 	"""Exibe a lista de ingredientes pedidos"""
-	print(toppings)
+	#print(toppings)
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+#make_pizza('pepperoni')
+#make_pizza('mushrooms', 'green peppers', 'extra cheese')
+	"""Apresentar a pizza que estamos prestes a preparar"""
+	print("\nMaking a pizza with the following toppings:")
 
+#for toppings in toppings:
+	print("- " + toppings)
+
+	make_pizza('pepperoni')
+	make_pizza('mushrooms', 'green peppers', 'extra cheese')
 
 
