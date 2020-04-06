e70018693cf7b20f97fddf385ecca0f8dff38562
renatogcruz <renatogcruz@hotmail.com>
2020-04-06 19:39:47 -0300

    passando núm. ar

---
+++ curso_intesivo_python/funcoes/pizza.py
@@ -0,0 +1,9 @@
+def make_pizza(*toppings): #o asterisco no nome diz a python para criar uma tupla vazia chamada toppings e reunir os valores recebidos nessa tupla
+	"""Exibe a lista de ingredientes pedidos"""
+	print(toppings)
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
+
