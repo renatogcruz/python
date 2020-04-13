ce5919a60953c6d587a588a146d633151b64b37d
renatogcruz <renatogcruz@hotmail.com>
2020-04-13 15:26:09 -0300

    definindo os atributos da classe

--- orientacao_objetos_py/namespaces/carro.py
+++ orientacao_objetos_py/namespaces/carro.py
@@ -1,4 +1,11 @@
 #criando classes
 
 class Carro():
-	
+	def __init__(self, cor, portas, combustivel, potencia):
+		#definido os atributos do carro
+		self.cor = cor
+		self.portas = portas
+		self.combustivel = combustivel
+		self.potencia
+
+
