85e58171b7df9020e2717e5f17b74cd283739176
renatogcruz <renatogcruz@hotmail.com>
2020-04-13 14:25:26 -0300

    add file 'namespace'

---
+++ orientacao_objetos_py/namespaces/namespaces.py
@@ -0,0 +1,11 @@
+#from math import  pow, factorial, log #primeira maneira de importação
+#from math import * #segunda maneira de importação
+import math #terceira maneira de importação (recomendável)
+
+#print(pow(2, 2))
+#print(factorial(5))
+#print(log(4, 2))
+
+print(math.pow(2, 2))
+print(math.factorial(5))
+print(math.log(4, 2))
