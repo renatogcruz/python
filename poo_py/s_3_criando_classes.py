358f10cee8eb658f13bb10f9bfc1c00baee23975
renatogcruz <renatogcruz@hotmail.com>
2020-05-04 09:39:37 -0300

    definindo atributo

--- poo_py/s_3_criando_classes.py
+++ poo_py/s_3_criando_classes.py
@@ -4,3 +4,20 @@ class MinhaClasse:
 obj = MinhaClasse()
 
 print(type(obj))
+
+class Ponto:
+
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def resetar(self):
+		self.x, self.y = 0, 0
+
+
+p = Ponto(10, 20)
+print(p.x, p.y)
+
+p.resetar()
+print(p.x, p.y)
+
