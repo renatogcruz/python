82c794cd12f4a4fe16f2b7cb3765f1e70f045d4a
renatogcruz <renatogcruz@hotmail.com>
2020-05-09 19:32:00 -0300

    duck typing - parte 2 - LBYL e EAFP

--- poo_py/s_3_duck_typing.py
+++ poo_py/s_3_duck_typing.py
@@ -2,7 +2,52 @@
 Se um objeto anda como um pato e faz quack como um pato então
 ele é um pato
 """
+class Pato:
 
+	def quack(self):
+		print("Quack, quack")
+
+class Pessoa:
+
+	def quack(self):
+		print("Eu faço quack igual a um pato")
+
+"""
+#isso NÃO é pythonico
+def fazer_quack(algo):
+
+	if isinstance(algo, Pato):
+		algo.quack()
+	else:
+		print("Isso tem que ser um pato")
+"""
+"""
+def fazer_quack(obj):
+	#LBYL - NÃO é pythonico
+	if hasattr(obj, 'quack'):
+		if callable(obj.quack):
+			obj.quack()
+"""
+
+def fazer_quack(obj):
+	# EAFP - Easier to ask for forgiveness than permission (pythonico)
+	try:
+		obj.quack()
+	except AttributeError as e:
+		print(e)
+
+
+
+
+pato = Pato()
+fazer_quack(pato)
+
+pessoa = Pessoa()
+fazer_quack(pessoa)
+
+
+
+"""
 class Livro():
 
 	def __init__(self, titulo, lancamento, autor):
@@ -20,3 +65,4 @@ class Filme():
 
 def imprimir(midia):
 	print(midia.titulo, midia.lancamento)
+"""
