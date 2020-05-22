3b7bf9bbb86fee7299dcecdcd019263ecaa2c327
renatogcruz <renatogcruz@hotmail.com>
2020-05-22 08:48:23 -0300

    criando uma instancia sem chamar o init

--- poo_py/s_3_dois_construtores.py
+++ poo_py/s_3_dois_construtores.py
@@ -1,15 +1,22 @@
 class Pessoa:
 
-	def __init__(self, nome):
+	def __init__(self, nome, idade):
 		self.nome = nome
+		self.idade = idade
 
 	#construtor alternativo
 	@classmethod
-	def outro_construtor(cls, nome):
-		return cls(nome)
+	def outro_construtor(cls, nome, idade):
+		return cls(nome, idade)
 
-#p = Pessoa('marcos')
+#p = Pessoa('marcos', 20)
 #print(p.nome)
 
-p = Pessoa.outro_construtor('pedro')
-print(p.nome)
+#p = Pessoa.outro_construtor('pedro', 30)
+#print(p.nome)
+
+p = Pessoa.__new__(Pessoa)
+dados = {'nome':'marcos', 'idade':28}
+for key, value in dados.items():
+	setattr(p, key, value)
+print(p.nome, p.idade)
