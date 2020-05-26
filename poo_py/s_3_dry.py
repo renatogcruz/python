acccec8f87d50e7f55cd61a49e45a012874a5972
renatogcruz <renatogcruz@hotmail.com>
2020-05-26 14:48:48 -0300

    lendo PEP8

--- poo_py/s_3_dry.py
+++ poo_py/s_3_dry.py
@@ -1,5 +1,27 @@
 """
 DRY = don't repeat youself
-
 cada parte do conhecimento deve ter uma representção unica, não ambigua e definitiva dentro do sistema
+
+PEP8 - Guia de estilo para o código Python
+link: https://wiki.python.org.br/GuiaDeEstilo
+
 """
+import os
+import sys
+
+r = minha_funcao(var_one, var_two,
+	             var_three, var_four)
+
+my_list = [
+     1, 2, 3,
+     4, 5, 6
+     ]
+
+result = (var1
+	      + var3
+	      + (var3 - var4)
+	      - result_var1
+	      - result_var4)
+
+foo(x, y)
+if x == 4: print(x)
