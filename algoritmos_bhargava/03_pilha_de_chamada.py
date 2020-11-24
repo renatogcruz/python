291ab29e34b256d24372432c018b92229727ddae
renatogcruz <renatogcruz@hotmail.com>
2020-11-24 08:11:20 -0300

    bhargava - a pilha de chamada

---
+++ algoritmos_bhargava/03_pilha_de_chamada.py
@@ -0,0 +1,15 @@
+#pilha de chamada
+
+def sauda(nome):
+	print("Olá, " + nome + "!")
+	sauda2(nome)
+	print ("preparando para dizer tchau...")
+	tchau()
+
+def sauda2(nome):
+	print("Como vai " + nome + "?")
+
+def tchau():
+	print("ok, tchau")
+
+sauda("Renato")
