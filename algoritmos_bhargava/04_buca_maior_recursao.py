a450474d273d07c1bce6cb3d50934740eb2db1d2
renatogcruz <renatogcruz@hotmail.com>
2020-11-28 12:27:02 -0300

    bhargava - achar o numeor maior com recursão

--- algoritmos_bhargava/04_buca_maior_recursao.py
+++ algoritmos_bhargava/04_buca_maior_recursao.py
@@ -8,3 +8,13 @@ def buscaMaior(arr):
 	return print(maior)
 
 buscaMaior([1, 2, 3, 4, 5, 6, 8])
+
+#com recursão
+
+def maximo(lista):
+	if len(lista) == 2:
+		return lista[0] if lista [0] > lista[1] else lista[1]
+	sub_max = maximo(lista[1:])
+	return lista[0] if lista[0] > sub_max else sub_max
+
+print(maximo([1, 2, 3, 4, 5, 6, 7, 8]))
