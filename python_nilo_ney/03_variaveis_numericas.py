a26e3045923a8fac9edd87b3d389cdd4b42346bc
renatogcruz <renatogcruz@hotmail.com>
2020-11-29 19:45:16 -0300

    nilo ney - entrando com números binários

--- python_nilo_ney/03_variaveis_numericas.py
+++ python_nilo_ney/03_variaveis_numericas.py
@@ -2,6 +2,7 @@ num = 1_000_000
 
 print(type(num))
 print(1_000_000 == 1000000.00)
+print(5.0 <= 5)
 #print(2 ** 1_000_000)
 print(3 * 0.1) # retorna 0.30000000000000004, ou seja, uma dizima periodica
 
