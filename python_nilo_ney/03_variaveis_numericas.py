c8a57648a8f02a1f5cd914094e7488847a136f69
renatogcruz <renatogcruz@hotmail.com>
2020-11-29 19:43:00 -0300

    nilo ney - problema dizima periodica

--- python_nilo_ney/03_variaveis_numericas.py
+++ python_nilo_ney/03_variaveis_numericas.py
@@ -2,3 +2,9 @@ num = 1_000_000
 
 print(type(num))
 print(1_000_000 == 1000000.00)
+#print(2 ** 1_000_000)
+print(3 * 0.1) # retorna 0.30000000000000004, ou seja, uma dizima periodica
+
+print(0b10) #base 2 - binário
+print(0x10) #base 16 - hexadecimal
+print(0o10) #base 8 - octal
