3143ce7aeaaf5433f440daa007cc225fef098c9d
renatogcruz <renatogcruz@hotmail.com>
2020-11-30 09:23:36 -0300

    nilo ney - expressões lógicas

--- python_nilo_ney/03_variaveis_numericas.py
+++ python_nilo_ney/03_variaveis_numericas.py
@@ -9,3 +9,8 @@ print(3 * 0.1) # retorna 0.30000000000000004, ou seja, uma dizima periodica
 print(0b10) #base 2 - binário
 print(0x10) #base 16 - hexadecimal
 print(0o10) #base 8 - octal
+
+print(True or False and not True) #avalia-se 'not', 'and' e depois 'or'
+print(True or False and False)    #avalia-se 'and' e depois 'or'
+
+print(100 > 1_000 and 20 > 18)    #operadores relacionais devem ser avaliados primeiro e depois as expressões lógicas
