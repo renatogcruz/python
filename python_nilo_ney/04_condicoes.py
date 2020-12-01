1034fd30aeed299db54eff3698ad53dd9b208316
renatogcruz <renatogcruz@hotmail.com>
2020-12-01 08:01:38 -0300

    nilo ney - condições if

--- python_nilo_ney/04_condicoes.py
+++ python_nilo_ney/04_condicoes.py
@@ -5,3 +5,10 @@ if a > b:
 	print("A é maior que B")
 else:
 	print("A não é maior que B")
+
+
+velocidade = 85
+
+if velocidade > 80:
+	print("Você foi multado")
+	print("valor da multa R$ {}" .format((velocidade - 80) * 5.00))
