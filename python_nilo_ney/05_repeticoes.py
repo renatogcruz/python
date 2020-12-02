7ca6745223e51debbf2cc28901a83efa35fe7cf6
renatogcruz <renatogcruz@hotmail.com>
2020-12-02 09:08:25 -0300

    nilo ney - estrutura de repetição - contadores

--- python_nilo_ney/05_repeticoes.py
+++ python_nilo_ney/05_repeticoes.py
@@ -6,4 +6,28 @@ while x <= 3:
 	x += 1
 
 #exercicios
+#5.1, 5.2
+x = 50
+while x <= 55:
+	print(x)
+	x += 1
+
+#5.3
+x = 10
+while x >= 1:
+	print(x)
+	x = x - 1
+
+print("\n")
+
+fim = 10
+x = 0
+while x <= fim:
+	if x % 2 == 0:
+		print(x)
+	x += 1
+
+while x <= fim:
+	print(x)
+	x += 2
 
