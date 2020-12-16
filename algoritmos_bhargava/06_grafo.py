d947d04c5250654c0da090fe6472b480ad06d9f7
renatogcruz <renatogcruz@hotmail.com>
2020-12-16 16:34:02 -0300

    bhargava - modelando grafos

---
+++ algoritmos_bhargava/06_grafo.py
@@ -0,0 +1,9 @@
+#implementando um grafo.
+#cada vértice é conectado com um vizinho
+#uma estrutura que conecta dados é uma tabela hash
+
+grafo = {}
+grafo["voce"] = ["alice", "bob", "claire"]
+
+print(grafo)
+
