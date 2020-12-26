ca83b03a04bd3b935f0d9572d257370b088672a2
renatogcruz <renatogcruz@hotmail.com>
2020-12-26 15:23:46 -0300

    nilo ney - exercício 6.5

---
+++ python_nilo_ney/06_listas_6_5.py
@@ -0,0 +1,24 @@
+##############################################################################
+# Parte do livro Introdução à Programação com Python
+# Autor: Nilo Ney Coutinho Menezes
+# Editora Novatec (c) 2010-2017
+# Primeira edição - Novembro/2010 - ISBN 978-85-7522-250-8
+# Primeira reimpressão - Outubro/2011
+# Segunda reimpressão - Novembro/2012
+# Terceira reimpressão - Agosto/2013
+# Segunda edição - Junho/2014 - ISBN 978-85-7522-408-3
+# Primeira reimpressão - Segunda edição - Maio/2015
+# Segunda reimpressão - Segunda edição - Janeiro/2016
+# Terceira reimpressão - Segunda edição - Junho/2016
+# Quarta reimpressão - Segunda edição - Março/2017
+# Terceira Edição - Janeiro/2019
+#
+# Site: http://python.nilo.pro.br/
+#
+# Arquivo: exercicios\capitulo 06\exercicio-06-05.py
+##############################################################################
+
+ultimo = 10
+fila = list(range(0, ultimo))
+
+print(fila)
