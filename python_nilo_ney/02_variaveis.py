efba7104d899bf10a419f5a8701a954b02f68a6b
renatogcruz <renatogcruz@hotmail.com>
2021-02-22 08:14:30 -0300

    python nilo - cap 2

---
+++ python_nilo_ney/02_variaveis.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# 2.1 primeiro programa com variáveis
+a = 2
+b = 3
+
+print(a + b)
+print(2 + 3)
+print(5)
+
+# 2.2 aumento de salário
+
+salario = 1500
+aumento = 5
+
+print( salario + (salario * aumento / 100))
+
+# 2.3 faça um programa que exiba seu nome na tela
+nome = "Renato Godoi da Cruz"
+
+print(nome)
+
+# 2.4 
+a = 3
+b = 5
+
+print((2 * a) * (3 * b))
+
+# 2.5 
+a = 5
+b = 7
+c = 10
+
+print(a + b + c)
+
+# 2.6
+salario = 750
+aumento = 15
+
+print(salario + (salario * 15 / 100))
