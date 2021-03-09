9b9b0c37bfe572f0b8b40516cd10bc955738457d
renatogcruz <renatogcruz@hotmail.com>
2021-03-09 10:22:22 -0300

    exercicios 6 python iax

---
+++ python_guia_IAexpert/read.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 08:28:19 2021
+
+@author: Renato
+"""
+
+def read_string():
+    string = str(input("Escreva sua mensagem: "))
+    return string
+
+def read_float():
+    valor = float(input("Escreva um valor: "))
+    return valor
+
+def concatenando(string, valor):
+    
+    return "string " + valor
+
