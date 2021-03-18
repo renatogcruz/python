e0e14498938a040e8d9433d4f4c6ec73d5eb268e
renatogcruz <renatogcruz@hotmail.com>
2021-03-18 13:49:25 -0300

    repetição - ex. 5.6, 5.7, 5.8 e 5.9

--- python_nilo_ney/05_7_revisao.py
+++ python_nilo_ney/05_7_revisao.py
@@ -2,6 +2,7 @@
 
 # 5.7 - repetições taboada
 
+print("Ex. 5.7 - repetição de taboada")
 
 n = int(input("Tabuada de: ")) 
 inicio = int(input("Entre com o início: "))
@@ -14,6 +15,9 @@ while inicio <= fim:
 
 
 # 5.8 2 numeros multiplicados utilizando soma
+
+print("\nEx. 5.8 multiplicação por soma")
+
 number1 = int(input("Entre com o primeiro número:  "))
 number2 = int(input("Entre com o segundo número:  "))
 
@@ -26,5 +30,45 @@ while cont <= number1:
 
 print("Resultado é ", soma)
     
+# 5.9 - leia 2 números e imprima a divisão do primeiro pelo segundo.
+
+print("\nEx. 5.9 - divisão por retirada")
+
+number1 = int(input("Entre com o primeiro número: "))
+number2 = int(input("Entre com o segundo número: "))
+
+cont = 0
+retirada = number1
+
+while number1 >= number2:
+    number1 -= number2
+    cont += 1
+    
+print("O resultado é ", cont)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 
