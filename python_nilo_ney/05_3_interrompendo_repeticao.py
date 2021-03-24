0afab8804eeedf54e19fa226fc8a1ccb7cf81703
renatogcruz <renatogcruz@hotmail.com>
2021-03-24 08:41:37 -0300

    máquina registradora

--- python_nilo_ney/05_3_interrompendo_repeticao.py
+++ python_nilo_ney/05_3_interrompendo_repeticao.py
@@ -32,7 +32,61 @@ while True:
     soma += valor
     contador += 1
 
-print(f"""Números digitados: {contador}, 
-      Soma: {soma}. 
-      Média: {soma / contador}""")
-      
+print(f"Números digitados: {contador}, Soma: {soma}.Média: {soma / contador}")
+
+
+# 5.15 Máquina registradora
+
+print("########################")
+print("# Máquina registradora #")     
+print("########################")   
+
+total = 0
+
+while True:
+    produto = int(input("Digite código do produto ou 0 para finalizar: "))
+    quantidade = int(input("Digite a quantidade de produto: "))
+    if produto == 0:
+        break
+    elif produto == 1:
+        total += 0.50 * quantidade 
+    elif produto == 2:
+        total += 1.00 * quantidade
+    elif produto == 3:
+        total += 4.00 * quantidade
+    elif produto == 5:
+        total += 7.00 * quantidade
+    elif produto == 9:
+        total += 8.00 * quantidade
+    else:
+        print('Valor inválido')
+
+print(f"Valor total: R$ {total:5.2f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
