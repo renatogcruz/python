90867678ddf8ec612f2301c37786cf89ec3fd569
renatogcruz <renatogcruz@hotmail.com>
2021-03-30 21:04:15 -0300

    modularização em Python

---
+++ Object_Oriented_Programming_with_Python/prog01.py
@@ -0,0 +1,16 @@
+class Class01:
+
+	def __init__(self):
+		print("Just created an objetc for Class01...")
+
+class Class02:
+
+	def __init__(self):
+		print("Just created an object for Class02...")
+
+def main():
+	o1 = Class01()
+	o2 = Class02()
+
+if __name__ == "__main__":
+	main()
