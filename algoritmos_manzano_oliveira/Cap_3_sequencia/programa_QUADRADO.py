1e64681a1d05fbc10bcc1bdce03ad87dcdeceba3
renatogcruz <renatogcruz@hotmail.com>
2021-04-15 08:11:30 -0300

    exercicios algoritmo sequencial

---
+++ algoritmos_manzano_oliveira/Cap_3_sequencia/programa_QUADRADO.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 08:01:36 2021
+
+@author: Renato
+
+4.z - ler dois números e apresentar o resultado inteirodo quadrado
+da divisão do valor da variável A em relação ao valor da variável 
+B armazenado em memória
+
+"""
+
+# Entrada
+a = int(input("Entre com o valor de A: "))
+b = int(input("Entre com valor de B: "))
+
+# Processamento
+r = (a / b) ** 2
+
+# Exibição
+print(f"O quadrado da divisão é igual a {r}")
+
+
---
+++ algoritmos_manzano_oliveira/Cap_3_sequencia/programa_SUCESSOR_ANTECESSOR.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 07:57:42 2021
+
+@author: Renato
+
+Ex. 4.y - construir um programa que leia um valor e apresente
+seu sucessor e seu antecessor
+
+"""
+
+# Entrada
+n = int(input("Entre com um valor: "))
+
+# Processamento
+s = n + 1
+a = n - 1
+
+# Exibição
+print(f"Antecessor = {a}. Sucessor = {s}")
