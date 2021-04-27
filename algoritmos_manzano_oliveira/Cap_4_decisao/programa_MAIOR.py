312f58fd330a154f3e89eff941ee035d57a2ffb7
renatogcruz <renatogcruz@hotmail.com>
2021-04-27 07:26:51 -0300

    exercícios algoritmos decisão

---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/programa_MAIOR.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 27 07:22:59 2021
+
+@author: Renato
+
+Ex. 3.l - página 109
+
+"""
+
+# Entrada
+n = int(input("Entre com valor qualquer: "))
+
+# Processamento
+if (not n > 3):
+    print(n)
---
+++ algoritmos_manzano_oliveira/Cap_4_decisao/programa_PERMITIDO.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 27 07:19:06 2021
+
+@author: Renato
+
+Ex. 3.k - página 109
+
+"""
+
+# Entrada 
+n = int(input("Entre com valor (entre 1 e 9): "))
+
+# Processamento
+if (n > 0 and n < 10):
+    print("O valor está na faixa permitida!")
+else:
+    print("O valor não está na faixa permitida!")
+
