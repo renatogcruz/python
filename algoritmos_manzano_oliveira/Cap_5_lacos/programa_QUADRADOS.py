0138b4100ff7e8d52a3f4a7ad774e15ee15ab0a6
renatogcruz <renatogcruz@hotmail.com>
2021-05-02 09:02:52 -0300

    exercicio algoritmo laço

---
+++ algoritmos_manzano_oliveira/Cap_5_lacos/programa_QUADRADOS.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  2 08:58:40 2021
+
+@author: Renato
+
+Exercicio A - página 141
+
+"""
+
+cont = 15
+
+# Processamento
+while cont <= 200:
+    qua = cont ** 2
+    print(f'{cont}² = {qua}')
+    cont = cont + 1
+    
