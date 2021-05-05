6c940d02d0b358abdb187ce62894f3c85cce7732
renatogcruz <renatogcruz@hotmail.com>
2021-05-05 14:18:34 -0300

    exercicio algoritmo leço

---
+++ algoritmos_manzano_oliveira/Cap_5_lacos/programa_INTEIROS_IMPARES.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  5 14:09:17 2021
+
+@author: Renato
+"""
+
+cont = 0
+
+while (cont <= 20):
+    resto = cont % 2
+    if resto != 0:
+        print(cont) 
+    cont += 1
+
+        
+        
