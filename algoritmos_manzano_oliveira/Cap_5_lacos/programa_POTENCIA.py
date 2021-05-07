48bf84d6331723222dccfcac0d5dd65520d389e6
renatogcruz <renatogcruz@hotmail.com>
2021-05-07 10:02:26 -0300

    exercício algoritmo laço

---
+++ algoritmos_manzano_oliveira/Cap_5_lacos/programa_POTENCIA.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  7 09:36:24 2021
+
+@author: Renato
+
+Exercício F - página 141
+
+"""
+
+# Entradas
+base = 3
+cont = 0
+
+# Processamento
+while cont <= 15:
+    resultado = base ** cont
+    print(f"{cont} = {resultado}")
+    cont += 1
+    
+
