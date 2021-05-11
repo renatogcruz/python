600e530fadf1bd949c61c8bf4f995dc5df59aecb
renatogcruz <renatogcruz@hotmail.com>
2021-05-11 08:33:53 -0300

    exercício algoritmo laço

---
+++ algoritmos_manzano_oliveira/Cap_5_lacos/programa_CELSIUS.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 08:29:55 2021
+
+@author: Renato
+
+Exercício J - página 142
+
+"""
+
+#  Entrada
+c = 10
+
+# Processamento
+while (c <= 100):
+    f = (c * 9 / 5) + 32
+    print(f'{c} ºC. {f} ºF')
+    c += 10
