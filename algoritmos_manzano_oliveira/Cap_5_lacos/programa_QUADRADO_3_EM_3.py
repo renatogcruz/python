0f8caf4cbb0f66e552d47ed695635de05a70d7c1
renatogcruz <renatogcruz@hotmail.com>
2021-05-17 08:33:58 -0300

    algoritmo laço

---
+++ algoritmos_manzano_oliveira/Cap_5_lacos/programa_QUADRADO_3_EM_3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 17 08:24:04 2021
+
+@author: Renato
+
+Exercício T - página 143
+
+"""
+
+# Entrada
+cont = 15
+
+# Processamento
+while (cont <= 200):
+    print(cont**2)
+    cont = cont + 3
+
