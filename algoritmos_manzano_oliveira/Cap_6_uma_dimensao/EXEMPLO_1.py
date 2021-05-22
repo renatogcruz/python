333ea3cba4a7abd401115f01a0c2b62869aa7b06
renatogcruz <renatogcruz@hotmail.com>
2021-05-22 14:50:23 -0300

    algoritmos dados uma dimensão

---
+++ algoritmos_manzano_oliveira/Cap_6_uma_dimensao/EXEMPLO_1.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 22 14:30:59 2021
+
+@author: Renato
+
+Exemplo 1 - página 151
+
+"""
+cont = 1
+lista = []
+
+while(cont <= 10):
+    valor = int(input("Entre com o valor: "))
+    lista.append(valor)
+    cont +=1
+   
+
+
+"""
+cont2 = 1
+par = []
+impar = []
+
+while(cont2 <= 10):
+    indice = lista[cont2]
+    resto = indice %2
+    if resto == 0:
+        multiplica = cont * 5
+        par.append(multiplica)        
+    else:
+        soma = cont + 5
+        impar.append(soma)
+    cont2 += 1
+        
+print(par)
+print(impar)
+"""
