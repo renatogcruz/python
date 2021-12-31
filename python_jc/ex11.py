74e9439515fe92d49110addef446218ebc604f8e
renatogcruz <renatogcruz@hotmail.com>
2021-12-31 12:50:16 -0300

    input e consultas no pydoc

---
+++ python_jc/ex11.py
@@ -0,0 +1,12 @@
+# página 39
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weigh = input()
+
+print(f"So, you're {age} old, {height} tall and {weigh} heavy.")
+
+language = input("Qual sua linguagem de programação favorita? ")
+print(f"Sua linguagem favorita é {language}")
